dc0632fee3a79fc9e60488bd25a97a02d9eea486
svenrdz <svenbmr@gmail.com>
2020-12-31 15:55:39 +0100

    fix linux linking

--- nimtralight.nim
+++ nimtralight.nim
@@ -3,7 +3,9 @@ export wrap, callback, string, config, settings
 
 {.passL: "-Lbuild".}
 when defined(macosx):
-  {.passL: "-rpath build"}
+  {.passL: "-rpath build".}
+elif defined(linux):
+  {.passL: "-Wl,-rpath,build".}
 {.passL: "-lUltralight".}
 {.passL: "-lUltralightCore".}
 {.passL: "-lAppCore".}
