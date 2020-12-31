6ff64b8b76b12e02a65d4b6265e886e4114165ac
svenrdz <svenbmr@gmail.com>
2020-12-31 01:17:08 +0100

    f

--- nimtralight.nim
+++ nimtralight.nim
@@ -2,6 +2,7 @@ import nimtralight/[wrap, callback, string, config, settings]
 export wrap, callback, string, config, settings
 
 # {.passL: "-Wl,-rpath -Wl,. -L."}
+{.passL: "-Lbuild".}
 {.passL: "-lUltralight".}
 {.passL: "-lUltralightCore".}
 {.passL: "-lAppCore".}
